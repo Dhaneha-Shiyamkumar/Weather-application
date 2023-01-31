c05aff57ce42d6893b0d88ff3f4e3470270fed8f
Neha <ran.danu789@gmail.com>
2023-01-31 16:05:56 +0530

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //WEATHER_API_KEY: '194333f5b09188fbda8c4a3bbfea30b2',
-  //TIME_API_KEY: 'GVK27JSUNMGC',
+  WEATHER_API_KEY: '00ec27eef01399a9262198a27fb26dfd',
+  TIME_API_KEY: 'EV1D5XZXADEX',
   GOOGLE_API_KEY: 'AIzaSyACBPmiVul-ihtnQBCz0u05uamNFdkfCNA',
   API_URL:'https://weather-app-backend2002.herokuapp.com/'
 };
